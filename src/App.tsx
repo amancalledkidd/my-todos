d9f616a0d9da33416409bf92f845fbc9346b0fa0
Kumani K <kumanikidd@gmail.com>
2024-01-04 14:38:06 +0000

    final adjustments to get prototype in line with customer spec

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import Nav from './components/Nav/Nav'
 import Input from './components/Input/Input'
 import TodoList from './components/TodoList/TodoList'
 import Todo from './types/Todo'
+import './App.scss'
 
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([])
 
   return (
-    <>
+    <div className='main-container'>
       <Nav setTodos={setTodos}/>
       <Input placeholder='Add your task here...' setTodos={setTodos} todos={todos}/>
       <TodoList todos={todos} setTodos={setTodos} />
-    </>
+    </div>
   )
 }
 
--- src/components/TodoCard/TodoCard.tsx
+++ src/components/TodoCard/TodoCard.tsx
@@ -11,17 +11,13 @@ type TodoCardProps = {
 
 const TodoCard = ({ todo, handleDelete, handleCheck }: TodoCardProps) => {
 
-    let content;
-    if (todo.complete) {
-        content = <p className='todo-card__todo'><s>{todo.text}</s></p>
-    } else {
-        content = <p className='todo-card__todo'>{todo.text}</p>
-    }
 
     return (
         <div className='todo-card'>
-            <input className='todo-card__checkbox' type="checkbox" checked={todo.complete} onChange={() => handleCheck(todo)} />
-            {content}
+            <label className='todo-card__item'>
+                <input className='todo-card__item__checkbox' type="checkbox" checked={todo.complete} onChange={() => handleCheck(todo)} />
+                <p className='todo-card__item__todo'>{todo.text}</p>
+            </label>
             <img className='todo-card__delete' src={bin} alt="delete" onClick={() => handleDelete(todo)} />
         </div>
     )
